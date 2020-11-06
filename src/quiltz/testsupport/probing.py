63683f6cb39b2f7470af49288d5b54441afd423f
rob <rob@qwan.eu>
2020-11-06 17:34:33 +0100

    improved probe_that to accept assertion or resul function

--- src/quiltz/testsupport/probing.py
+++ src/quiltz/testsupport/probing.py
@@ -1,12 +1,36 @@
 from time import sleep
-import logging
+from hamcrest import assert_that
+from hamcrest.core.matcher import Matcher
 
-def probe_that(f, timeout=1000):
+def probe_that(assertion_or_delayed_result_function, matcher=None, timeout=1000):
+    """probes the delayed_result_function until it matches the hamcrest matcher given.
+    if it matches the matcher within the timeout the probe passes. It if does not it fails 
+    with an AssertionError, just like assert_that would.
+
+    probe_that can be called in two ways: 
+    1: with a delayed result function (callable) and a matcher
+    example:
+        probe_that(lambda: result, equal_to(5))
+
+    2: with a delayed assertion
+    examples:
+        probe_that(lambda: assert result == 5)
+        probe_that(lambda: assert_that(result, equal_to(5)))
+
+    :param assertion_or_delayed_result_function:  a callable executing the assertion or returning actual result
+    :param matcher:   matcher to be used in mode 1
+    :param timeout:   timeout (default one second)
+    """
+    assertion = assertion_or_delayed_result_function
+    if matcher is not None: 
+        if not isinstance(matcher, Matcher):
+            raise(ValueError("matcher is not a matcher"))
+        assertion = lambda: assert_that(assertion_or_delayed_result_function(), matcher)
     t = 0
     success = False
     while(not success):
         try:
-            f()
+            assertion()
             success = True
         except AssertionError as e:
             sleep(50.0/1000.0)
--- tests/quiltz/testsupport/test_probing.py
+++ tests/quiltz/testsupport/test_probing.py
@@ -1,6 +1,6 @@
 from testing import *
 from quiltz.testsupport import probe_that, log_collector
-from hamcrest import raises
+from hamcrest import raises, calling
 import time
 
 class TestProbeThat_with_assert_that:
@@ -9,6 +9,19 @@ class TestProbeThat_with_assert_that:
 
     def test_passes_on_assert_that_passes(self):
         probe_that(lambda: assert_that(service_returning_true_eventually(), is_(True)))
+
+class TestProbeThat_without_assert_that_and_with_a_matcher:
+    def test_fails_on_assert_that_fails(self):
+        assert_that(calling(probe_that).with_args(service_returning_true_eventually, is_(False)), raises(AssertionError))
+
+    def test_passes_on_assert_that_passes(self):
+        probe_that(service_returning_true_eventually, is_(True))
+
+    def test_fails_with_argument_error_when_matcher_is_not_really_a_matcher(self):
+        assert_that(
+            calling(probe_that).with_args(service_returning_true_eventually, 123), 
+            raises(ValueError, "matcher is not a matcher"))
+
 
 def service_returning_true_eventually(delay=0.02):
     time.sleep(delay)
